92c6d637d0b327246ab7f6ede6823fb23526d665
Adit Karode <adit.karode@gmail.com>
2026-08-16 11:02:13 -0700

    refactor(briefstate): extract watermark core

--- internal/mora/lease_guard.go
+++ internal/mora/lease_guard.go
@@ -2,12 +2,9 @@ package mora
 
 import (
 	"github.com/pyranthus-hq/mora/internal/leasefile"
-	"os"
 )
 
 func leaseGuardPath(lockPath string) string { return leasefile.GuardPath(lockPath) }
 func withLeaseFileGuard(lockPath string, fn func() error) error {
 	return leasefile.WithGuard(lockPath, fn)
 }
-func unlockLeaseGuard(f *os.File) error  { return leasefile.Unlock(f) }
-func tryLockLeaseGuard(f *os.File) error { return leasefile.TryLock(f) }
